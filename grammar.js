5d3947e1541b99139ae28dfb37f89d4c4f4348f9
robng <49914302+robng@users.noreply.github.com>
2026-02-23 17:48:36 +0100

    feat: match namespaces (baby steps)

--- grammar.js
+++ grammar.js
@@ -7,11 +7,40 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+/**
+ * Creates a case-insensitive `RustRegex` from the given
+ * string and returns it.
+ * @param s {string}
+ */
+function ci(s) {
+  return new RustRegex("(?i)" + s);
+}
+
 export default grammar({
   name: "xsharp",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ =>
+      repeat($.namespace),
+
+    namespace: $ => seq(
+      $.kw_begin,
+      $.kw_namespace,
+      $.identifier,
+      // TODO: optional($.namespace_body),
+      $.kw_end,
+      $.kw_namespace,
+    ),
+
+    identifier: _ =>
+      new RustRegex(".*"),
+
+    kw_begin: _ =>
+      ci("begin"),
+    kw_end: _ =>
+      ci("end"),
+
+    kw_namespace: _ =>
+      ci("namespace"),
   }
 });
